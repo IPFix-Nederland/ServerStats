cd9445532c47c64e8a00c372dbb77effb23eef96
Scott Verhage <info@scottverhage.nl>
2023-09-09 22:36:29 +0200

    Push it

---
+++ src/main/java/com/scottverhage/serverstats/ServerStats.java
@@ -0,0 +1,17 @@
+package com.scottverhage.serverstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
